370d358f412a9a0b9d86595d9d286eefa008d9d2
strawpoint <strawpoint@outlook.com>
2024-03-15 16:11:40 -0500

    actualized
    faltan links

--- form.php
+++ form.php
@@ -1,8 +1,14 @@
 <?php
-$name = $_POST[''];
-$visitor_email = $_POST[''];
-$subject = $_POST[''];
-$message = $_POST[''];
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['text'];
+
+$grabar = fopen("com.html","a");
+fwrite ($grabar.""<p>$comentario</p>);
+fclose($grabar);
+
+include("blog.html");
+
 
 
 
